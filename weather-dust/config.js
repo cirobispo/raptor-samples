a946a61b9b26c3a891819dbf8da8e10230ed9870
Patrick Steele-Idem <pnidem@gmail.com>
2014-06-18 11:21:25 -0600

    Added support for mock services

--- weather-dust/config.js
+++ weather-dust/config.js
@@ -35,13 +35,34 @@ function parseArgs() {
         .parse();  
 }
 
-exports.useMockData = false;
+function enableMockServices() {
+    var fs = require('fs');
+    var servicesMockDir = nodePath.join(__dirname, 'src/services-mock');
+    var servicesDir = nodePath.join(__dirname, 'src/services');
+    fs.readdirSync(servicesMockDir).forEach(function(filename) {
+        if (/-mock.js$/.test(filename)) {
+            console.log('Enabling mock service for "' + filename + '"...');
+            // This is a mock service module and we want to overwrite
+            // methods in the non-mock version of the module
+            var mockModule = require(nodePath.join(servicesMockDir, filename));
+            var targetModule = require(nodePath.join(servicesDir, filename.slice(0, 0-'-mock.js'.length) + '.js'));
 
+            for (var k in mockModule) {
+                if (mockModule.hasOwnProperty(k)) {
+                    targetModule[k] = mockModule[k];
+                }
+            }
+        }
+    });
+
+}
 
 exports.load = function(callback) {
     var args = parseArgs();
 
-    exports.useMockData = args.mockData === true;
+    if (args.mockData === true) {
+        enableMockServices();
+    }
 
     var configDir = nodePath.join(__dirname, 'config');
     confit(configDir).create(function(err, config) {
