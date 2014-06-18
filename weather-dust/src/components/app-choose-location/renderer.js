5db6de7cc3819f84333ed4e02d04fb3ab00e2341
Patrick Steele-Idem <pnidem@gmail.com>
2014-06-18 14:01:38 -0600

    String module path for widget is now allowed in template

--- weather-dust/src/components/app-choose-location/renderer.js
+++ weather-dust/src/components/app-choose-location/renderer.js
@@ -1,9 +1,7 @@
 var template = require('view-engine').load(require.resolve('./template.dust'));
-var widgetPath = require.resolve('./widget');
 
 module.exports = function render(data, context) {
     template.render({
-        widgetId: data.widgetId,
-        widgetPath: widgetPath
+        widgetId: data.widgetId
     }, context);
 };
--- weather-dust/src/components/app-weather/renderer.js
+++ weather-dust/src/components/app-weather/renderer.js
@@ -1,9 +1,7 @@
 var template = require('view-engine').load(require.resolve('./template.dust'));
-var widgetPath = require.resolve('./widget');
 
 module.exports = function render(data, context) {
     template.render({
-        weatherData: data.weatherData,
-        widgetPath: widgetPath
+        weatherData: data.weatherData
     }, context);
 };
