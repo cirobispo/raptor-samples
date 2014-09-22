7cfac1ecc5e2603dd94657beef64a4b9d7a4f2ce
Patrick Steele-Idem <pnidem@gmail.com>
2014-09-22 11:25:24 -0600

    Removed whitespace

--- weather/src/pages/home/index.js
+++ weather/src/pages/home/index.js
@@ -3,14 +3,14 @@ var weatherService = require('../../services/weather');
 
 module.exports = function(req, res) {
     res.setHeader('Content-Type', 'text/html; charset=utf-8');
-    
+
     var location = req.params.location || req.query.location;
 
     function weatherDataProvider(arg, callback) {
         if (!location) {
             return callback();
         }
-        
+
         weatherService.getCurrentWeather({query: location}, callback);
     }
 
