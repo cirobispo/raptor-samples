117be69c7c7733e5a8aaa628d8c8bf0e13f4fa27
Patrick Steele-Idem <pnidem@gmail.com>
2014-09-22 14:01:37 -0600

    Updated sample app to use latest Express

--- marko-express/server.js
+++ marko-express/server.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var serveStatic = require('serve-static');
+
 var indexTemplate = require('marko').load(require.resolve('./index.marko'));
 var app = express();
 var port = 8080;
 
-app.use('/static', express.static(__dirname + '/static'));
+app.use('/static', serveStatic(__dirname + '/static'));
 
 app.get('/', function(req, res) {
     indexTemplate.stream({
